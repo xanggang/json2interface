5b16c8590350926f4c103df68b66cb2f77604258
lynn <1057516363@qq.com>
2021-04-14 18:51:11 +0800

    feat: 修改部署选项

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   lintOnSave: true, // eslint 错误处理，true表示对待eslint错误为warnings，warnings不会导致编译失败
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/json2interface/'
+    : '/'
 }
